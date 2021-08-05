0f878a4213db6ac9921c582f2a03c4ddf13f9638
liuwei <2544853876@qq.com>
2021-08-05 20:59:23 +0800

    提交用户测试8

--- cache/cache-spring-boot-starter/src/main/java/com/liu/cachespringbootstarter/handler/CacheHandler.java
+++ cache/cache-spring-boot-starter/src/main/java/com/liu/cachespringbootstarter/handler/CacheHandler.java
@@ -21,6 +21,6 @@ public class CacheHandler {
         Object returnObj = methodInvocation.proceed();
         redisCacheManager.put(able.key(),returnObj);
         return returnObj;
-
+        return returnObj;
     }
 }
