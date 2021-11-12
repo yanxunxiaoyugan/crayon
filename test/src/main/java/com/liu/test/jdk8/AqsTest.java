ebd31bba51babdd0a10e432390e82c1bc7ad2ee5
yanxunxiaoyugan <1852564730@qq.com>
2021-11-12 18:05:30 +0800

    添加aqs

--- test/src/main/java/com/liu/test/jdk8/AqsTest.java
+++ test/src/main/java/com/liu/test/jdk8/AqsTest.java
@@ -5,27 +5,37 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class AqsTest {
     public static void main(String[] args) {
-
+        Thread main = Thread.currentThread();
         ReentrantLock lock = new ReentrantLock(true);
         try{
             lock.lock();
             Thread thread = new Thread(() ->{
+                try {
+                    main.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 lock.lock();
             });
             thread.start();
-
+//            thread.join();
             Thread thread1 = new Thread(() ->{
+                try {
+                    main.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 lock.lock();
             });
             thread1.start();
-
-            lock.lock();
+//            thread1.join();
+//            lock.lock();
 
 
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-//            lock.unlock();
+            lock.unlock();
 //            lock.unlock();
         }
         boolean locked = lock.isLocked();
