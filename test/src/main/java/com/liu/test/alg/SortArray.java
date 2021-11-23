cbf00ce428ebbbbbddf2cc5a011b55cce39c69cd
yanxunxiaoyugan <1852564730@qq.com>
2021-11-23 20:00:58 +0800

    补充mysql

---
+++ test/src/main/java/com/liu/test/alg/SortArray.java
@@ -0,0 +1,59 @@
+package com.liu.test.alg;
+
+public class SortArray {
+    public static void main(String[] args) {
+        SortArray solution = new SortArray();
+        int[] arr = new int[]{3,2,4,4,5,6,4,7};
+        solution.sortArray(arr);
+        for(int i : arr){
+            System.out.print(i+" ");
+        }
+    }
+    public int[] sortArray(int[] nums) {
+        if(nums == null || nums.length == 1){
+            return nums;
+        }
+        quickSort(nums,0,nums.length - 1);
+        return nums;
+    }
+
+    private void quickSort(int[] nums, int left, int right){
+        if(left >= right){
+            return ;
+        }
+        int[] indexs = partition(nums,left,right);
+        quickSort(nums,left,indexs[0] -1);
+        quickSort(nums,indexs[1] + 1,right);
+    }
+    private int[] partition(int[] nums, int left, int right){
+        int[] res = new int[2];
+        int low = left;
+        int high = right;
+        int current = left;
+        int temp = nums[right];
+        while(current < high){
+            if(nums[current] > temp){
+                swap(nums,current,high);
+                high--;
+            }else if(nums[current] < temp){
+                swap(nums,current,low);
+                current++;
+                low++;
+            }else{
+                current++;
+            }
+        }
+//        swap(nums,right,high);
+        res[0] = low ;
+        res[1] = high ;
+
+        return res;
+
+    }
+
+    private void swap(int[] nums,int m,int n){
+        int temp = nums[m];
+        nums[m] = nums[n];
+        nums[n] = temp;
+    }
+}
---
+++ test/src/main/java/com/liu/test/alg/SortArray2.java
@@ -0,0 +1,49 @@
+package com.liu.test.alg;
+
+public class SortArray2 {
+    public static void main(String[] args) {
+        SortArray2 solution = new SortArray2();
+        int[] arr = new int[]{3,2,4,4,5,6,4,7};
+        solution.quickSort(arr);
+        for(int i : arr){
+            System.out.print(i+" ");
+        }
+    }
+    public static void quickSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    public static void quickSort(int[] arr, int l, int r) {
+        if (l < r) {
+//            swap(arr, l + (int) (Math.random() * (r - l + 1)), r);
+            int[] p = partition(arr, l, r);
+            quickSort(arr, l, p[0] - 1);
+            quickSort(arr, p[1] + 1, r);
+        }
+    }
+
+    public static int[] partition(int[] arr, int l, int r) {
+        int less = l - 1;
+        int more = r;
+        while (l < more) {
+            if (arr[l] < arr[r]) {
+                swap(arr, ++less, l++);
+            } else if (arr[l] > arr[r]) {
+                swap(arr, --more, l);
+            } else {
+                l++;
+            }
+        }
+        swap(arr, more, r);
+        return new int[] { less + 1, more };
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+}
